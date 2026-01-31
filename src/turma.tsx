3d84e05c9ade81d64a7357ba160c78cb793f52ac
Rodrigo <rodrigopegneau@gmail.com>
2026-01-31 07:32:10 +0100

    third parameter on menos

--- src/turma.tsx
+++ src/turma.tsx
@@ -2,6 +2,6 @@ function add(a: number, b: number) {
   return a + b;
 }
 
-function menos(a: number, b: number) {
-  return a - b;
+function menos(a: number, b: number, c: number) {
+  return a - b - c;
 }
